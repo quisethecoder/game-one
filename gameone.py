d537913f2696bc6c7a94e69b279a184d0cf8a7c9
Marquise <marquise_dawkins@aol.com>
2024-12-01 17:54:39 -0500

    logic update/fix

--- gameone.py
+++ gameone.py
@@ -6,7 +6,18 @@
 import random
 
 name_one = input("Player one, what is your name?: ")
+
+while name_one.strip() == "":
+    if name_one.strip() == "":
+        print("Please enter your name")
+        name_one = input("Player one, what is your name?: ")
+
 name_two = input("Player two, what is your name?: ")
+
+while name_two.strip() == "":   
+    if name_two.strip() == "":
+        print("Player two, please enter your name")
+        name_two = input("Player two, what is your name?: ")
 
 menu_player_one = (f"""{name_one}, Choose an option.
 1. Rock
@@ -15,6 +26,15 @@ menu_player_one = (f"""{name_one}, Choose an option.
 4. Choose for me
 Select [1-4]: """)
 
+option_player_one = input(menu_player_one)
+
+while option_player_one not in ["1","2","3","4"]:
+    if option_player_one not in ["1","2","3","4"]:
+        print(f"{name_one}, please enter a valid option for player 1, you chose {option_player_one}")
+        option_player_one = input(menu_player_one)
+
+
+
 menu_player_two = (f"""{name_two}, Choose an option.
 1. Rock
 2. Paper
@@ -22,16 +42,15 @@ menu_player_two = (f"""{name_two}, Choose an option.
 4. Choose for me
 select [1-4]: """)
 
-option_player_one = input(menu_player_one)
 option_player_two = input(menu_player_two)
 
-while option_player_one not in ["1","2","3","4"] or option_player_two not in ["1","2","3","4"]:
-    if option_player_one not in ["1","2","3","4"]:
-        print(f"{name_one}, please enter a valid option for player 1, you chose {option_player_one}")
-        option_player_one = input(menu_player_one)
+while option_player_two not in ["1","2","3","4"]:
     if option_player_two not in ["1","2","3","4"]:
         print(f"{name_two}, please enter a valid option for player 2, you chose {option_player_two}")
         option_player_two = input(menu_player_two)
+
+
+
 
 option_player_one = int(option_player_one)
 option_player_two = int(option_player_two)
